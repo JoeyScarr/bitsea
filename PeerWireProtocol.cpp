5ee92109bb21f94f219da8592c25790b6e2138fc
tuanchien <tuan@sdk.org.nz>
2015-07-16 22:42:04 +1200

    Implement network read timeout and slightly more peer wire implementation.

--- PeerWireProtocol.cpp
+++ PeerWireProtocol.cpp
@@ -7,6 +7,10 @@ bool PeerClient::createPeerConnection() {
 	handleNetworking();
 }
 
+void PeerClient::keepAlive() {
+	lastReceivedMessage = time(nullptr);
+}
+
 void PeerClient::initHandshakeMessage() {
 	const std::string pstr = "BitTorrent protocol"; // version 1.0 of BT protocol.
 
@@ -15,11 +19,11 @@ void PeerClient::initHandshakeMessage() {
 	for(int i=0; i < 8; i++)
 		reserved[i] = '\0';
 	
-	PeerClient::handshake += pstrlen;
-	PeerClient::handshake += pstr;
-	PeerClient::handshake += reserved;
-	PeerClient::handshake += info_hash;
-	PeerClient::handshake += peer_id;
+	handshake += pstrlen;
+	handshake += pstr;
+	handshake += reserved;
+	handshake += info_hash;
+	handshake += peer_id;
 }
 
 void PeerClient::sendHandshake() {
@@ -30,41 +34,159 @@ void PeerClient::sendHandshake() {
 	catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+	
+	keepAlive();
+}
+
+size_t PeerClient::networkRead(std::array<std::uint8_t, NETWORK_BUFFER_SIZE> &buf, asio::error_code &error) {
+	return socket.read_some(asio::buffer(buf), error);
 }
 
 void PeerClient::handleNetworking() {
 	try{
-		std::array<char, NETWORK_BUFFER_SIZE> buf;
+		std::array<std::uint8_t, NETWORK_BUFFER_SIZE> buf;
 		asio::error_code error;
 		size_t len;
 		
 		while(true) {
-			len = socket.read_some(asio::buffer(buf), error);
+			std::function<size_t()> boundNetworkRead = std::bind(&PeerClient::networkRead, this, std::ref(buf), std::ref(error));
+			std::future<size_t> future = std::async(boundNetworkRead);
+			std::future_status status;
+			status = future.wait_for(std::chrono::seconds(CONNECTION_TIMEOUT_LIMIT));
+			
+			if(status == std::future_status::timeout) {
+				std::cerr << "Peer " + peer_id + " timed out.\n";
+				break;
+			}
 			
 			if (error == asio::error::eof)
 				break; // Connection closed cleanly by peer.
 			else if (error)
 				throw asio::system_error(error); // Some other error.
 
+			len = future.get();
 			processNetworkData(buf, len);
 		}
 	}
 	catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+	
+	socket.close();
 }
 
-void PeerClient::processNetworkData(std::array<char, NETWORK_BUFFER_SIZE> buffer, size_t length) {
-	char *data = buffer.data();
-	std::uint8_t *networkData = reinterpret_cast<std::uint8_t *>(data);
+void PeerClient::processNetworkData(std::array<std::uint8_t, NETWORK_BUFFER_SIZE> buffer, size_t length) {
+	std::uint8_t *networkData = buffer.data();
 	std::uint32_t messageLength = *reinterpret_cast<std::uint32_t *>(networkData);
 	messageLength = ntohl(messageLength);
-	
+	std::uint8_t *payload = networkData+5;
+
 	if(messageLength == 0) {
-		PeerClient::keepAlive();
+		keepAlive();
 		return;
 	}
-	
-	std::uint8_t messageId = networkData[4];
-	
+
+	std::uint8_t messageId = networkData[3];
+	messageId = ntohl(messageId);
+	processMessage(messageId, payload, messageLength-1);
 }
+
+void PeerClient::processMessage(std::uint8_t messageId, std::uint8_t *data, int payloadSize) {
+		switch(messageId) {
+		case mId.choke:
+			choke();
+			break;
+		case mId.unchoke:
+			unchoke();
+			break;
+		case mId.interested:
+			interested();
+			break;
+		case mId.notInterested:
+			notInterested();
+			break;
+		case mId.have:
+			have(data, payloadSize);
+			break;
+		case mId.bitfield:
+			bitfield(data, payloadSize);
+			break;
+		case mId.request:
+			request(data, payloadSize);
+			break;
+		case mId.piece:
+			piece(data, payloadSize);
+			break;
+		case mId.cancel:
+			cancel(data, payloadSize);
+			break;
+		case mId.port:
+			port(data, payloadSize);
+			break;
+	}
+}
+
+	void PeerClient::choke() {
+		status.choked = true;
+	}
+		
+	void PeerClient::unchoke() {
+		status.choked = false;
+	}
+	
+	void PeerClient::interested() {
+		status.interested = true;
+	}
+	
+	void PeerClient::notInterested() {
+		status.interested = false;
+	}
+	
+	void PeerClient::have(std::uint8_t *data, int length) {
+		std::uint32_t piece = *reinterpret_cast<std::uint32_t *>(data);
+		piece = ntohl(piece);
+	}
+	
+	void PeerClient::bitfield(std::uint8_t *data, int length) {
+		if(status.bitfield.size() != 0)
+			throw "Bitfield already set for peer: " + peer_id;
+			
+		for(int i=0; i < length; i++) 
+			status.bitfield.push_back(data[i]);
+	}
+	
+	void PeerClient::request(std::uint8_t *data, int length) {
+		std::uint32_t index = *reinterpret_cast<std::uint32_t *>(data[0]);
+		std::uint32_t begin = *reinterpret_cast<std::uint32_t *>(data[4]);
+		std::uint32_t len = *reinterpret_cast<std::uint32_t *>(data[8]);
+		
+		index = ntohl(index);
+		begin = ntohl(begin);
+		len = ntohl(len);
+	}
+	
+	void PeerClient::piece(std::uint8_t *data, int length) {
+		std::uint32_t index = *reinterpret_cast<std::uint32_t *>(data[0]);
+		std::uint32_t begin = *reinterpret_cast<std::uint32_t *>(data[4]);
+		index = ntohl(index);
+		begin = ntohl(begin);
+		
+		std::vector<uint8_t> block;
+		for(int i=0; i < length-9; i++)
+			block.push_back(data[8+i]);
+	}
+	
+	void PeerClient::cancel(std::uint8_t *data, int length) {
+		std::uint32_t index = *reinterpret_cast<std::uint32_t *>(data[0]);
+		std::uint32_t begin = *reinterpret_cast<std::uint32_t *>(data[4]);
+		std::uint32_t len = *reinterpret_cast<std::uint32_t *>(data[8]);
+		
+		index = ntohl(index);
+		begin = ntohl(begin);
+		len = ntohl(len);
+	}
+	
+	void PeerClient::port(std::uint8_t *data, int length) {
+		std::uint32_t listenPort = *reinterpret_cast<std::uint32_t *>(data[0]);
+		listenPort = ntohl(listenPort);
+	}
--- PeerWireProtocol.hpp
+++ PeerWireProtocol.hpp
@@ -4,32 +4,44 @@
 #include <array>
 #include <asio.hpp>
 #include <asio/ip/tcp.hpp>
+#include <time.h>
+#include <memory>
+#include <vector>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <asio/use_future.hpp>
 #include "Tracker.hpp"
 
 class PeerClient {
 private:
 
-	struct MessageIds {
-		const uint8_t choke = 0;
-		const uint8_t unchoke = 1;
-		const uint8_t interested = 2;
-		const uint8_t notInterested = 3;
-		const uint8_t have = 4;
-		const uint8_t bitfield = 5;
-		const uint8_t request = 6;
-		const uint8_t piece = 7;
-		const uint8_t cancel = 8;
-		const uint8_t port = 9;
-	}
-
-	static const int NETWORK_BUFFER_SIZE = 1024;
-	bool choked;
-	bool interested;
-	bool am_choking;
-	bool am_interested;
-	bool peer_choking;
-	bool peer_interested;
+	static const struct MessageId {
+		static const uint8_t choke = 0;
+		static const uint8_t unchoke = 1;
+		static const uint8_t interested = 2;
+		static const uint8_t notInterested = 3;
+		static const uint8_t have = 4;
+		static const uint8_t bitfield = 5;
+		static const uint8_t request = 6;
+		static const uint8_t piece = 7;
+		static const uint8_t cancel = 8;
+		static const uint8_t port = 9;
+	} mId;
 	
+	static const int NETWORK_BUFFER_SIZE = 1024;
+	static const int CONNECTION_TIMEOUT_LIMIT = 120;
+	
+	struct Status {
+		bool choked;
+		bool interested;
+		bool am_choking;
+		bool am_interested;
+		bool peer_choking;
+		bool peer_interested;
+		std::vector<std::uint8_t> bitfield;
+	} status;
+
 	std::string sendBuffer;
 	std::string handshake;
 	const std::string &info_hash;
@@ -41,25 +53,42 @@ private:
 	asio::ip::tcp::socket socket;
 	asio::ip::tcp::resolver::iterator endpoint_iterator;
 	
+	time_t lastReceivedMessage;
+	
 public:
 	PeerClient(const std::string &hash, const std::string &id, const std::string &hostname, const std::string &port)
 	: info_hash(hash), peer_id(id), resolver(io_service), 
 	  socket(io_service), query(hostname, port) {
-		am_choking = 1;
-		am_interested = 0;
-		peer_choking = 1;
-		peer_interested = 0;
+		status.am_choking = 1;
+		status.am_interested = 0;
+		status.peer_choking = 1;
+		status.peer_interested = 0;
 		endpoint_iterator = resolver.resolve(query);
 	}
 	
 	void handleNetworking();
-	void processNetworkData(std::array<char, NETWORK_BUFFER_SIZE> buffer, size_t length); // process input from readNetworkData.
+	size_t networkRead(std::array<std::uint8_t, NETWORK_BUFFER_SIZE> &buf, asio::error_code &error);
+	void processNetworkData(std::array<std::uint8_t, NETWORK_BUFFER_SIZE> buffer, size_t length); // process input from readNetworkData.
 	void sendHandshake();
 	bool createPeerConnection();
 	bool startListener();
 	void initHandshakeMessage();
 	void keepAlive();
+	void processMessage(std::uint8_t messageId, std::uint8_t *data, int payloadSize);
+	
+	void choke();
+	void unchoke();
+	void interested();
+	void notInterested();
+	void have(std::uint8_t *data, int length);
+	void bitfield(std::uint8_t *data, int length);
+	void request(std::uint8_t *data, int length);
+	void piece(std::uint8_t *data, int length);
+	void cancel(std::uint8_t *data, int length);
+	void port(std::uint8_t *data, int length);
 };
+
+const int PeerClient::CONNECTION_TIMEOUT_LIMIT;
 
 class PeerServer {
 };
